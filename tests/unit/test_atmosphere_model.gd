1ba46c461740056e08ac2a9b3feea0959dd4dfa2
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 02:02:45 +0500

    test: cover atmospheric density behavior

---
+++ tests/unit/test_atmosphere_model.gd
@@ -0,0 +1,19 @@
+class_name TestAtmosphereModel
+extends TestCase
+
+
+func test_sea_level_and_negative_altitude_use_sea_level_density() -> void:
+	var atmosphere := AtmosphereModel.new()
+	TestAssert.is_near(atmosphere.density_at_altitude(0.0), 1.225, 0.000001)
+	TestAssert.is_near(atmosphere.density_at_altitude(-100.0), 1.225, 0.000001)
+
+
+func test_density_decreases_with_altitude() -> void:
+	var atmosphere := AtmosphereModel.new()
+	var sea_level := atmosphere.density_at_altitude(0.0)
+	var five_km := atmosphere.density_at_altitude(5000.0)
+	var ten_km := atmosphere.density_at_altitude(10000.0)
+
+	TestAssert.is_true(five_km < sea_level)
+	TestAssert.is_true(ten_km < five_km)
+	TestAssert.is_true(ten_km > 0.0)
