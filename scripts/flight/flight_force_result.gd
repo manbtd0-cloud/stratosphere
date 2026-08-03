ad19197996c450286555bb0ca7c6af49e7dfcc7c
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 02:01:56 +0500

    feat: add flight force result contract

---
+++ scripts/flight/flight_force_result.gd
@@ -0,0 +1,9 @@
+class_name FlightForceResult
+extends RefCounted
+
+var total_force_world: Vector3 = Vector3.ZERO
+var total_torque_world: Vector3 = Vector3.ZERO
+var thrust_force_world: Vector3 = Vector3.ZERO
+var drag_force_world: Vector3 = Vector3.ZERO
+var lift_force_world: Vector3 = Vector3.ZERO
+var gravity_force_world: Vector3 = Vector3.ZERO
