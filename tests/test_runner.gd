02321a51f364e2b5ee0cc47f3aa9f94745aa1d9f
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 02:25:14 +0500

    test: register asset manifest validation suite

--- tests/test_runner.gd
+++ tests/test_runner.gd
@@ -11,6 +11,7 @@ const TEST_SUITES := [
 	preload("res://tests/unit/test_flight_camera_rig.gd"),
 	preload("res://tests/integration/test_flight_room_loop.gd"),
 	preload("res://tests/unit/test_flight_hud.gd"),
+	preload("res://tests/unit/test_asset_manifest_validator.gd"),
 ]
 
 
