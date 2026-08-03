8d771976f85165f9fac3a16c0325193e3b18943d
manbtd0-cloud <manbtd0@gmail.com>
2026-08-04 02:07:19 +0500

    refactor: expose stable flight force result contract

--- scripts/flight/flight_force_result.gd
+++ scripts/flight/flight_force_result.gd
@@ -1,9 +1,12 @@
 class_name FlightForceResult
 extends RefCounted
 
-var total_force_world: Vector3 = Vector3.ZERO
-var total_torque_world: Vector3 = Vector3.ZERO
+var force_world: Vector3 = Vector3.ZERO
+var torque_world: Vector3 = Vector3.ZERO
 var thrust_force_world: Vector3 = Vector3.ZERO
 var drag_force_world: Vector3 = Vector3.ZERO
 var lift_force_world: Vector3 = Vector3.ZERO
 var gravity_force_world: Vector3 = Vector3.ZERO
+var thrust_newtons: float = 0.0
+var drag_newtons: float = 0.0
+var lift_newtons: float = 0.0
